61c462123f95764f0ddb07d37dfb8b5fae2d229c
yusril <99790793+yusrilrnld@users.noreply.github.com>
2022-05-19 16:11:35 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -3,44 +3,44 @@ from userbot.utils import edit_or_reply, ram_cmd
 from userbot import CMD_HELP, CMD_HANDLER as cmd, owner
 eor = edit_or_reply
 
-@ram_cmd(pattern='oi(?: |$)(.*)')
+@ram_cmd(pattern='pay(?: |$)(.*)')
 async def _(event):
-    await eor(event, f"`Hai Perkenalkan Namaku {owner}`")
-    sleep(3)
-    await eor(event, "Salam Kenal Ya semua`")
+    await eor(event, f"`Sedang Menulis Payment {owner}`")
+    sleep(2)
+    await eor(event, "`Mohon Tunggu.....`")
     sleep(1)
-    await eor(event, "`Kalian Semua kontol, Ngentot, Anak Yatim, Anak haram, Anak lonte, Ga berguna, Penyembah Tongkat kera sakti. :)`")
-# Create by myself @localheart
+    await eor(event, "`**PAYMENT ACENG • STOREE \n\n DANA = 0895611203477 \n\nㅤPAYMENT ACENG • STOREE \n\nㅤGOPAY = 081270603368 \n\nㅤ★@ACENG_STOREE★ **",
+    # Create by myself @YUSRIL4YOU
 
 
-@ram_cmd(pattern='sayang(?: |$)(.*)')
+@ram_cmd(pattern='pay2(?: |$)(.*)')
 async def _(event):
     await eor(event, "`Cuma Mau Bilang`")
     sleep(3)
     await eor(event, "`Aku Sayang Kamu`")
     sleep(1)
     await eor(event, "`I LOVE YOU 💞`")
-# Create by myself @localheart
+# Create by myself @yusril4you
 
 
-@ram_cmd(pattern='semangat(?: |$)(.*)')
+@ram_cmd(pattern='pay3(?: |$)(.*)')
 async def _(event):
     await eor(event, "`Apapun Yang Terjadi`")
     sleep(3)
     await eor(event, "`Tetaplah Bernapas`")
     sleep(1)
     await eor(event, "`Dan Selalu Bersyukur`")
-# Create by myself @localheart
+# Create by myself @yusril4you
 
 CMD_HELP.update(
     {
-       "kenalan": f"**Plugin :** Kenalan.\
-       \n\n    • Syntax : `{cmd}oi`\
-       \n     • **Function: **Untuk memperkenalkan diri Mu.\
-       \n\n    •  Syntax : `{cmd}sayang`\
-       \n     • **Function: **Untuk mengungkapkan Rasa.\
-       \n\n    •  Syntax : `{cmd}semangat`\
-       \n     • **Function: **Untuk menyemangati seseorang.\
+       "payments": f"**Plugin :** payments.\
+       \n\n    • Syntax : `{cmd}pay`\
+       \n     • **Function: **Untuk Menulis Payments.\
+       \n\n    •  Syntax : `{cmd}pay2`\
+       \n     • **Function: **Untuk Menulis Payments.\
+       \n\n    •  Syntax : `{cmd}pay3`\
+       \n     • **Function: **Untuk Menulis Payments.\
     "
     }
 )
